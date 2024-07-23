d4503ecae66106a5ef6888e1ec5b738c1f6c6eae
Harikrishna Mallasani <hkrishna6225@gmail.com>
2024-07-24 00:49:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+import SetAvatar from "./pages/SetAvatar";
+import UserProfile from "./pages/UserProfile";
+import ForgotPassword from "./pages/Forgot-Password";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/setAvatar" element={<SetAvatar />} />
+          <Route path="/*" element={<Chat />} />
+          <Route path="/users/id=:id" element={<UserProfile />} />
+          <Route path="/forgotPassword" element={<ForgotPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
